467345891e5518cd7333c7869640bec5ace7e6aa
Faris0520 <faris.6dsdiaf@gmail.com>
2024-07-26 00:00:09 +0800

    finalize the UI

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ async function checkGender() {
             const response = await fetch(`https://api.genderize.io/?name=${nama}`);
             const data = await response.json();
             if (data.gender) {
-                resultElement.innerHTML = `<strong>${nama}</strong> is <strong>${data.gender}</strong> with <strong>${data.probability * 100}%</strong> certainty.`;
+                const emoji = data.gender === 'male' ? '👨' : '👩';
+                resultElement.innerHTML = `${emoji} <strong>${nama}</strong> is <strong>${data.gender}</strong> with <strong>${data.probability * 100}%</strong> certainty.`;
             } else {
-                resultElement.textContent = `Uh oh. <strong>${nama}</strong> is unknown to us`;
+                resultElement.innerHTML = `Uh oh. <strong>${nama}</strong> is unknown to us`;
             }
         } catch (error) {
             resultElement.textContent = 'An error occurred while fetching the data.';
         }
     } else {
         resultElement.textContent = 'Please enter a name.';
+    }
+}
+
+function kirim(event) {
+    if (event.key === 'Enter') {
+        checkGender();
     }
 }
